91cbf8314d0d55da8274222cb20439532aa5649b
amaj98 <amaj1998@gmail.com>
2019-09-22 19:34:51 -0400

    basic outline of game ready

---
+++ wordwizard.js
@@ -0,0 +1,16 @@
+var startGame = ()=>{
+    $(document).keydown(inputHandler);
+}
+
+var inputHandler = (evt)=>{
+    if(evt.keyCode== 13){
+        //submit for input
+        $('#play').text("");
+    }
+    else if (evt.keyCode >= 65 && evt.keyCode<=90){
+        $('#play').text(($('#play').text() + evt.key).toUpperCase());
+    }
+    else if(evt.keyCode == 8){
+        $('#play').text($('#play').text().slice(0,-1));
+    }
+}
