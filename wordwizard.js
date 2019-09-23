59458e1378c7308c87452d4e9f3d28f5779ca6dd
amaj98 <amaj1998@gmail.com>
2019-09-23 01:09:18 -0400

    test

--- wordwizard.js
+++ wordwizard.js
@@ -21,9 +21,8 @@ var startGame = ()=>{
     $('#game').append("<div id = 'guesses'></div>");
     $('#game').append("<div id = 'letterbank'>LETTERS</div>");
     let letters = ""
-    $.get("api.php",(data)=>console.log(data["rack"]));
-    console.log(letters);
-    $('#letterbank').append("<div class = 'letters text-center' id = 'letters'>" + letters + "</div>");
+    $.get("api.php",(data)=>letters =data["rack"]);
+    $('#letterbank').append("<div class = 'letters text-center' id = 'letters'>" + [...letters].join(' ') + "</div>");
     $(document).keydown(inputHandler);
 }
 
