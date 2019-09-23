aa7c4291513441570d3d990d3c41ba70a73f735a
amaj98 <amaj1998@gmail.com>
2019-09-23 01:51:27 -0400

    added combinations function

--- wordwizard.js
+++ wordwizard.js
@@ -24,8 +24,22 @@ var startGame = ()=>{
     jQuery.ajaxSetup({async:false});
     let letters = ""
     $.get("api.php",(data)=>letters = data["rack"]);
+    let combos = combinations(letters);
+    console.log(combos);
     $('#letterbank').append("<div class = 'letters text-center' id = 'letters'>" + [...letters].join(' ') + "</div>");
     $(document).keydown(inputHandler);
+}
+
+var combinations = (letters)=>{
+    let combos = [];
+    let f = (prefix, letters)=> {
+      for (var i = 0; i < letters.length; i++) {
+        result.push(prefix + letters[i]);
+        f(prefix + letterss[i], letters.slice(i + 1));
+      }
+    }
+    f('', letters);
+    return combos;
 }
 
 var inputHandler = (evt)=>{
