09ad25062b1f7e4fa65634ab0de3b79e6b7a6788
amaj98 <amaj1998@gmail.com>
2019-09-23 18:20:53 -0400

    letters working again

--- wordwizard.js
+++ wordwizard.js
@@ -25,9 +25,9 @@ var startGame = ()=>{
 }
 
 var processRack = (rack) => {
-    //let combos = combinations(rack);
-    //console.log(JSON.stringify(combos));
-    //$.post("api.php",{"words":JSON.stringify(combos)},(data)=>processWords(data));
+    let combos = combinations(rack);
+    console.log(JSON.stringify(combos));
+    $.post("api.php",{"words":JSON.stringify(combos)},(data)=>processWords(data));
     $('#letterbank').append("<div class = 'letters text-center' id = 'letters'>" + [...rack].join(' ') + "</div>");
     
 }
