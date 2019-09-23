2eccd3c9a0bad970821555efe1843009f9791e62
amaj98 <amaj1998@gmail.com>
2019-09-22 23:57:46 -0400

    test

--- api.php
+++ api.php
@@ -1,68 +1,31 @@
 <?php
 
-
-
-    //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
-
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
 
     if (!$dbhandle) die ($error);
 
- 
-
-    //this is a sample query which gets some data, the order by part shuffles the results
-
-    //the limit 0, 10 takes the first 10 results.
-
-    // you might want to consider taking more results, implementing "pagination", 
-
-    // ordering by rank, etc.
-
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
-
-    
-
-    //this next line could actually be used to provide user_given input to the query to 
-
-    //avoid SQL injection attacks
-
-    $statement = $dbhandle->prepare($query);
-
-    $statement->execute();
-
-    
-
-    //The results of the query are typically many rows of data
-
-    //there are several ways of getting the data out, iterating row by row,
-
-    //I chose to get associative arrays inside of a big array
-
-    //this will naturally create a pleasant array of JSON data when I echo in a couple lines
-
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    
-
-    //this part is perhaps overkill but I wanted to set the HTTP headers and status code
-
-    //making to this line means everything was great with this request
 
     header('HTTP/1.1 200 OK');
+    header('Content-Type: application/json');
 
-    //this lets the browser know to expect json
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-    //header('Content-Type: application/json');
+        $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
+        $statement = $dbhandle->prepare($query);
+    
+        $statement->execute();
+    
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode($results);
+        }
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $test = json_decode($_POST);
         echo json_encode($_POST);
     }
 
     
-    elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        echo json_encode($results);
-    }
+
 
 
 
