80d1e36a83772ebc44804c2fb0bba145cf12b112
amaj98 <amaj1998@gmail.com>
2019-09-22 22:52:44 -0400

    test

--- wordwizard.js
+++ wordwizard.js
@@ -2,7 +2,7 @@ var ready = () =>{
     $('body').empty();
     $('body').text('abc');
     $('body').on('click',()=>{
-        $.ajax({method: "GET",url:"api.php",success: (data)=>$('body').text(JSON.parse(data))})
+        $.ajax({method: "GET",url:"api.php",success: (data)=>$('body').text(JSON.stringify(data))})
     });
     /*
     $('#game').text("Press Enter to start")
