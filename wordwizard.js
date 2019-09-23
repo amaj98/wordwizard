042bb0ddb69e2ff79cc2c43491654a6750661237
amaj98 <amaj1998@gmail.com>
2019-09-23 03:59:03 -0400

    test

--- wordwizard.js
+++ wordwizard.js
@@ -1,7 +1,7 @@
 var ready = () =>{
     $('body').empty();
     $('body').text('abc');
-    $('body').on('click',()=>{$.post("api.php",'["A","B","AB"]',(data)=>$('body').text(JSON.stringify(data)))});
+    $('body').on('click',()=>{$.post("api.php",'["A","B","AB"]',(data)=>$('body').text("post"))});
     $('body').on('keydown',()=>{$.get("api.php",(data)=>$('body').text(JSON.stringify(data)))});
     
     
