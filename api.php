464cd62591fd96b1975e19b8cd8a6abba9a21b4f
amaj98 <amaj1998@gmail.com>
2019-09-23 03:24:50 -0400

    php fixes

--- api.php
+++ api.php
@@ -22,7 +22,7 @@
         $combos = json_decode($_POST);
         $words = array();
         foreach ($combos as $key => $value) {
-            $query = "SELECT words FROM racks WHERE rack ="+$value;
+            $query = "SELECT words FROM racks WHERE rack = '$value'"
             $statement = $dbhandle->prepare($query);
             $statement->execute();
             $raw = $statement->fetch(PDO::FETCH_ASSOC);
