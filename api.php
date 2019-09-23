f9389b83d99c0fb3436fa7ad500e2b949e40a626
amaj98 <amaj1998@gmail.com>
2019-09-23 00:21:40 -0400

    test

--- api.php
+++ api.php
@@ -10,9 +10,8 @@
 
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-        $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
+        $query = "SELECT rack FROM racks WHERE length=7 and weight <= 10 order by random() limit 1";
         $statement = $dbhandle->prepare($query);
-    
         $statement->execute();
     
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
